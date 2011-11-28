ebfac8d74cb8c83f795ee190a515ee70d4983732
Kevin <kevin@colorz.fr>
2011-11-28 12:01:04 +0100

    Presentation

--- index.php
+++ index.php
@@ -32,9 +32,23 @@ if(
 <html lang="fr-FR">
     <head>
 	<meta charset="UTF-8" />
-	<title>Title</title>
+	<title>Give Me RGBA !</title>
+	<style>
+	body {
+		position:relative;
+	}
+	body,
+	h1 {
+		margin:0;
+		padding:0;
+	}
+	a img {
+		border:none;
+	}
+	</style>
     </head>
     <body>
+		<h1>Give Me RGBA !</h1>
 		<form action="" method="post">
 			<ul>
 				<?php if(!empty($content)) : ?>
@@ -56,5 +70,7 @@ if(
 				</li>
 			</ul>
 		</form>
+		<a href="http://github.com/darklg/GiveMeRGBA"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/7afbc8b248c68eb468279e8c17986ad46549fb71/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" /></a>
+		
     </body>
 </html>
